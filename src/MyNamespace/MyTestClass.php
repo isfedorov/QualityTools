fe6b26dfcdae5f22fabe1812178ef48694336c8c
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2020-10-15 14:18:10 +0300

    configs

--- src/MyNamespace/MyTestClass.php
+++ src/MyNamespace/MyTestClass.php
@@ -5,5 +5,13 @@ namespace MyNamespace;
 
 class MyTestClass
 {
+    private $Some_Long_Name_Of_property;
+
+
+    public function __construct($Some_Long_Name_Of_property)
+    {
+        $this->Some_Long_Name_Of_property = $Some_Long_Name_Of_property;
+    }
+
 
 }
