e3ae83ee63965643f4350f5e5fd72d52eb2a36fa
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2022-07-09 09:56:51 +0200

    add custom rules

--- rector.php
+++ rector.php
@@ -1,13 +1,14 @@
 <?php
 
+use QualityTools\rules\MyFirstRector;
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
     $rectorConfig->phpVersion(PhpVersion::PHP_82);
-
-    $rectorConfig->sets([LevelSetList::UP_TO_PHP_82, SetList::PHP_82]);
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_82, SetList::PHP_82, SetList::PHP_81]);
+    $rectorConfig->rule(MyFirstRector::class);
 };
---
+++ src/rules/Com_Example_Rule_NoFunctions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace QualityTools\rules;
+
+class Com_Example_Rule_NoFunctions extends \PHPMD\AbstractRule
+{
+    public function apply(\PHPMD\AbstractNode $node)
+    {
+        // Check constraints against the given node instance
+    }
+}
